9129907d1b2a221a38e8270d859fd52a08c6eb71
shermainesy <shermaine_sy@dlsu.edu.ph>
2015-12-02 20:32:39 +0800

    log log in debug debug

--- app/src/main/java/com/example/atayansy/tot/URL/url.java
+++ app/src/main/java/com/example/atayansy/tot/URL/url.java
@@ -4,5 +4,5 @@ package com.example.atayansy.tot.URL;
  * Created by shermainesy on 11/30/15.
  */
 public class url {
-    public static String ip = "http://localhost:8081/ToT";
+    public static String ip = "http://localhost:8081/ToT/";
 }
