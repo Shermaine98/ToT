fad66697531e5a9ec8adbaefe08eb947cde548d6
shermainesy <shermaine_sy@dlsu.edu.ph>
2015-12-02 23:37:59 +0800

    fix codes

--- app/src/main/java/com/example/atayansy/tot/MainActivity.java
+++ app/src/main/java/com/example/atayansy/tot/MainActivity.java
@@ -5,7 +5,6 @@ import android.content.SharedPreferences;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -16,34 +15,32 @@ import android.widget.Toast;
 
 import com.example.atayansy.tot.URL.url;
 import com.example.atayansy.tot.java.User;
-import com.google.gson.Gson;
 import com.squareup.okhttp.FormEncodingBuilder;
 import com.squareup.okhttp.OkHttpClient;
 import com.squareup.okhttp.Request;
 import com.squareup.okhttp.RequestBody;
 import com.squareup.okhttp.Response;
 
-import org.json.JSONException;
-import org.json.JSONObject;
-
 import java.io.IOException;
-import java.util.concurrent.TimeUnit;
+
 
 public class MainActivity extends AppCompatActivity {
-
-    //change this
-
+    /*Views*/
     Button btnSignIn;
     TextView btnSignUp;
+    /*OBJECTS*/
     User user;
+    /*OTHER*/
     SharedPreferences sp;
-    private EditText userName, password;
+    /*Another Page*/
     View.OnClickListener redirect = new View.OnClickListener() {
         @Override
         public void onClick(View v) {
             Intent i = new Intent();
             if (v.equals(btnSignIn)) {
-                new Login().execute(userName.getText().toString(), password.getText().toString());
+                /** Connect to Database **/
+                new SignIn().execute();
+
             } else if (v.equals(btnSignUp)) {
                 i.setClass(getBaseContext(), SignUp.class);
                 startActivity(i);
@@ -51,6 +48,7 @@ public class MainActivity extends AppCompatActivity {
             }
         }
     };
+    private EditText userName, password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -64,10 +62,16 @@ public class MainActivity extends AppCompatActivity {
         btnSignIn = (Button) findViewById(R.id.bt_signInPage);
         btnSignUp = (TextView) findViewById(R.id.bt_signUpPage);
 
+        /* Calling a Method*/
+
         btnSignIn.setOnClickListener(redirect);
         btnSignUp.setOnClickListener(redirect);
 
     }
+
+    /**
+     * CODES THAT ARE NOT CHANGED
+     **/
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -91,83 +95,53 @@ public class MainActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    private class Login extends AsyncTask<String, Void, String> {
+    /*DATABASE*/
+    private class SignIn extends AsyncTask<String, Void, String> {
+        User currentUser = new User();
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            currentUser.setUserName(userName.getText().toString());
+            currentUser.setPassword(password.getText().toString());
+        }
 
         @Override
         protected String doInBackground(String... params) {
 
-            OkHttpClient client = new OkHttpClient();
+            OkHttpClient okHttpClient = new OkHttpClient();
+
+            RequestBody requestbody = new FormEncodingBuilder().
+                    add("username", currentUser.getUserName()).
+                    add("password", currentUser.getPassword()).build();
+
+            Request request = new Request.Builder().url(url.ip + "LoginServlet").post(requestbody).build();
+
             Response response = null;
-            client.setConnectTimeout(100, TimeUnit.SECONDS);
-
-            RequestBody requestBody = new FormEncodingBuilder()
-                    .add("username", params[0]).add("password", params[1])
-                    .build();
-            Log.i("link", params[0]);
-            Log.i("link", params[1]);
-            Log.i("requestBody", requestBody.toString());
-
-            Request r = new Request.Builder().url(url.ip + "LoginServlet").post(requestBody).build();
-            Log.i("Request", r.toString());
-
-
             try {
-                response = client.newCall(r).execute();
+                response = okHttpClient.newCall(request).execute();
             } catch (IOException e) {
                 e.printStackTrace();
             }
-
-            Log.i("link", response.toString());
-            Log.i("urlconnect", "example");
-
 
             String result = "";
 
             try {
                 result = response.body().string();
-                Log.i("result", result);
             } catch (IOException e) {
-
+                e.printStackTrace();
             }
+
 
             return result;
         }
-
         @Override
         protected void onPostExecute(String s) {
             super.onPostExecute(s);
-
-            try {
-                JSONObject jo = new JSONObject(s);
-
-                user = new User(jo.getInt("userID"), jo.getString("username"), jo.getString("email"), jo.getString("password"));
-                Log.i("user: ", user.getUserID() + "");
-                Log.i("user: ", user.getUserName() + "");
-                Log.i("user: ", user.getPassword() + "");
-                Log.i("user: ", user.getEmail() + "");
-
-            } catch (JSONException e) {
-            }
-            if (user != null) {
-                sp = getSharedPreferences("login", MODE_PRIVATE);
-
-                SharedPreferences.Editor editor = sp.edit();
-                Gson gson = new Gson();
-                String json = gson.toJson(user);
-                editor.putString("user", json);
-
-                editor.commit();
-
-
-                Intent i = new Intent();
-                i.setClass(getBaseContext(), MainActivity.class);
-
-                startActivity(i);
-                finish();
-            } else {
-                Toast.makeText(getBaseContext(), "Invalid Username/Password!", Toast.LENGTH_LONG).show();
-            }
+            Toast.makeText(getBaseContext(), "ENTER", Toast.LENGTH_LONG).show();
         }
+
+
     }
 
 }
